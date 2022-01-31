27acf6bee27537dff8c7952214055f3295dbb8ee
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2022-01-31 11:07:38 -0500

    Generate tower

--- generate.py
+++ generate.py
@@ -1,1 +1,19 @@
-pass
+import pyrosim.pyrosim as pyrosim 
+
+pyrosim.Start_SDF("boxes.sdf")
+length = width = height = 1
+x = y = z = 0
+for y in range(5):
+	for x in range(5):
+		for num in range(10):
+			pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
+			z += 1
+			width = width * .90
+			length = length * .90
+			height = height * .90
+		x += 1
+		width = length = height = 1
+		z = 0
+	y += 1
+	x = 0
+pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,8 +1,15 @@
 import pybullet as p
 import time
+import pybullet_data
+
+#Alters how world is simulated
 
 physicsClient = p.connect(p.GUI)
-for value in range(1000):
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
+for value in range(1000000000000):
 	p.stepSimulation()
 	time.sleep(1/60)
 	print(value)
