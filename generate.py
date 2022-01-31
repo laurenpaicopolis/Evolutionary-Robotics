4484205cd3bc8abe09d50af3c2b4c373d31eaf58
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2022-01-31 17:51:16 -0500

    Created robot with joints and links

--- generate.py
+++ generate.py
@@ -1,19 +1,28 @@
 import pyrosim.pyrosim as pyrosim 
-
-pyrosim.Start_SDF("boxes.sdf")
 length = width = height = 1
 x = y = z = 0
-for y in range(5):
-	for x in range(5):
-		for num in range(10):
-			pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-			z += 1
-			width = width * .90
-			length = length * .90
-			height = height * .90
-		x += 1
-		width = length = height = 1
-		z = 0
-	y += 1
-	x = 0
-pyrosim.End()
+def create_world():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[4, 4, 0], size=[length, width, height])
+    pyrosim.End()
+
+def create_robot():
+    '''pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[x, y, 0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0, 0, 1.0])
+    pyrosim.Send_Cube(name="Link1", pos=[0, 0, 0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link2_Link1", parent="Link1", child="Link2", type="revolute", position=[0, 0, 1.0])
+    pyrosim.Send_Cube(name="Link2", pos=[0, 0, 0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link3_Link2", parent="Link2", child="Link3", type="revolute", position=[0, 0.5, 0.5])
+    pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0], size=[length, width, height])'''
+
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, y, 1.5], size=[length, width, height]) # root link
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, y, -0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, y, -0.5], size=[length, width, height])
+    pyrosim.End()
+
+create_world()
+create_robot()
